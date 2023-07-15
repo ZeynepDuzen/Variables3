aa6f8364f08bbea5d4b4341765472dca3857d80e
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-15 11:11:35 +0300

    Proje dosyası ekle.

---
+++ Variables3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir dikdörtgenin çevresini ve alanını hesaplayın
+            int kisaKenar, uzunKenar, cevre, alan;
+            kisaKenar = 3;
+            uzunKenar = 5;
+            cevre = 2*(kisaKenar + uzunKenar);
+            alan = kisaKenar * uzunKenar;
+
+            Console.WriteLine("Çevre : " + cevre);
+            Console.Write("Alan : " + alan);
+
+            Console.Read();
+
+        }
+    }
+}
